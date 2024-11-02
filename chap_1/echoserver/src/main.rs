6039bb5780361ea20cd4df62955bc2bb9f54bbc3
masaki12-s <masaki.sanada@yappli.co.jp>
2024-11-03 00:50:31 +0900

    [add] label

--- chap_1/echoserver/src/main.rs
+++ chap_1/echoserver/src/main.rs
@@ -14,15 +14,15 @@ fn echo_server(address: &str) -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind(address)?; // [1]
     loop {
         let (mut stream, _) = listener.accept()?; // [2]
-        thread::spawn(move || {
+        thread::spawn(move || { // [3]
             let mut buffer = [0u8; 1024];
             loop {
-                let nbytes = stream.read(&mut buffer).unwrap();
-                if nbytes == 0 {
+                let nbytes = stream.read(&mut buffer).unwrap(); // [4]
+                if nbytes == 0 { [6]
                     return;
                 }
                 print!("{}", str::from_utf8(&buffer[..nbytes]).unwrap());
-                stream.write_all(&buffer[..nbytes]).unwrap();
+                stream.write_all(&buffer[..nbytes]).unwrap(); // [5]
             }
         });
     }
